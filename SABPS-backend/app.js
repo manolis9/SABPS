738b08dadb585fc56fbc79256d545bd77e52aaa9
manolis9 <manolis@alumni.ubc.ca>
2016-10-11 16:41:01 -0700

    Backend logic sending booking confirmation and booking completion emails to the user

---
+++ SABPS-backend/app.js
@@ -0,0 +1,57 @@
+var firebase = require('firebase');
+var nodemailer = require('nodemailer');
+
+// firebase.initializeApp({
+// 	serviceAccount: "./app/SABPS-595760d743f6.json",
+// 	databaseURL: "https://sabps-cd1b7.firebaseio.com"
+
+// });
+
+var config = {
+	apiKey: "AIzaSyAq4VZqZ2yC5JLioLbdLRIJ2JPh0oW1r7w",
+	authDomain: "sabps-cd1b7.firebaseapp.com",
+	databaseURL: "https://sabps-cd1b7.firebaseio.com",
+	storageBucket: "sabps-cd1b7.appspot.com",
+	messagingSenderId: "881638063284"
+};
+firebase.initializeApp(config);
+
+var smtpConfig = {
+    host: 'smtp.office365.com',
+    port: 587,
+    secure: false, // use SSL
+    auth: {
+        user: 'manolis.ioannides@mazdis.com',
+        pass: 'Joannakrupa_9'
+    }
+};
+
+var transporter = nodemailer.createTransport(smtpConfig);
+
+firebase.database().ref().child('Emails to Send').on('child_changed', function(emailSnap) {
+	var email = emailSnap.val();
+	//sendEmailHelper(email.from, email.to, email.subject, email.body);
+	console.log(email.from);
+	console.log(email.to);
+	console.log(email.subject);
+	console.log(email.body);
+
+	var mailOptions = {
+		from: email.from.toString(), // sender address
+		to: email.to.toString(), // list of receivers
+		subject: email.subject.toString(), // Subject line
+		text: email.body.toString(), // plaintext body
+		html: '<b>' + email.body.toString()+ '</b>' // html body
+	}
+
+	transporter.sendMail(mailOptions, function(error, info) {
+		if (error) {
+			return console.log(error);
+		}
+		console.log('Message sent: ' + info.response);
+	});
+
+	// Remove it now that we've processed it.
+	// emailSnap.ref().remove();
+
+});
